cf98ede02cad52d5dd12e53771cbb3fda3146d9c
Andrew Stewart <1andrewstewart@gmail.com>
2020-09-27 13:58:41 -0400

    enquirer questions added

--- app.js
+++ app.js
@@ -4,12 +4,136 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+const appendFileAsync = util.promisify(fs.appendFile);
+const generateManager = require("./utils/generateManager.js");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const managerQuestions = [
+  {
+    name: "name",
+    message: "What's your manager's name?",
+    type: "input",
+  },
+  {
+    name: "id",
+    message: "What's your manager's ID number?",
+    type: "input",
+  },
+  {
+    name: "email",
+    message: "What's your manager's email?",
+    type: "input",
+  },
+  {
+    name: "officeNum",
+    message: "What's your manager's office number?",
+    type: "input",
+  },
+  {
+    name: "add",
+    message: "Add an additional team member?",
+    type: "list",
+    choices: ["Add Engineer", "Add Intern", "I'm done adding team members"],
+  },
+];
+
+const engineerQuestions = [
+  {
+    name: "name",
+    message: "What's your engineer's name?",
+    type: "input",
+  },
+  {
+    name: "id",
+    message: "What's your engineer's ID number?",
+    type: "input",
+  },
+  {
+    name: "email",
+    message: "What's your engineer's email?",
+    type: "input",
+  },
+  {
+    name: "github",
+    message: "What's your engineer's GitHub username?",
+    type: "input",
+  },
+  {
+    name: "add",
+    message: "Add an additional team member?",
+    type: "list",
+    choices: ["Add Engineer", "Add Intern", "I'm done adding team members"],
+  },
+];
+
+const internQuestions = [
+  {
+    name: "name",
+    message: "What's your intern's name?",
+    type: "input",
+  },
+  {
+    name: "id",
+    message: "What's your intern's ID number?",
+    type: "input",
+  },
+  {
+    name: "email",
+    message: "What's your intern's email?",
+    type: "input",
+  },
+  {
+    name: "school",
+    message: "Which school does your intern attend?",
+    type: "input",
+  },
+  {
+    name: "add",
+    message: "Add an additional team member?",
+    type: "list",
+    choices: ["Add Engineer", "Add Intern", "I'm done adding team members"],
+  },
+];
+
+function init() {
+  inquirer
+    .prompt(managerQuestions)
+    .then((answers) => {
+      console.log(answers);
+    //   console.log(JSON.parse(answers));
+      appendFileAsync("team.html", generateManager({...answers}))
+        .then(() => {
+          console.log("Written to file");
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+
+    //   if (answers.add === "Add Engineer") {
+    //     inquirer.prompt(engineerQuestions).then((answers) => {
+    //       console.log(answers);
+ 
+    //     });
+    //   } else if (answers.add === "Add Intern") {
+    //     inquirer.prompt(internQuestions).then((answers) => {
+    //       console.log(answers);
+
+    //     });
+    //   } else {
+    //   }
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log("We have a problem!");
+    });
+}
+
+init();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
